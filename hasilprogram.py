8aee9909cd4aec5f8dbe6bf22a07ada63fe766a9
Hadeyal <hadeyalgans@gmail.com>
2021-10-27 10:54:13 +0700

    Menambahkan File

---
+++ hasilprogram.py
@@ -0,0 +1,9 @@
+Python 3.8.10 (tags/v3.8.10:3d8993a, May  3 2021, 11:34:34) [MSC v.1928 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+= RESTART: C:/Users/DELL/Documents/Program Menghitung Luas dan keliling Lingkaran.py
+Program Menghitung Luas dan Keliling Lingkaran
+Masukan Jari - Jari =4.6
+Luas Lingkaran Adalah      = 66.50
+Luas Keliling Adalah       = 28.91
+>>> 
